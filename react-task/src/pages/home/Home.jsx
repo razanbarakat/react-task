09a1483462e8586bdd76f9a07eddebb6151c5408
razanbarakat <razan.alfeelat@gmail.com>
2024-10-16 23:10:22 +0300

    rendering array of data

--- react-task/src/pages/home/Home.jsx
+++ react-task/src/pages/home/Home.jsx
@@ -1,42 +1,60 @@
 import Header from "../../comp/header";
 import Footer from "../../comp/Footer";
 import Loading from "../../comp/Loading";
-import Erroe404 from '../erroe404';
+import Erroe404 from "../erroe404";
 import { Helmet } from "react-helmet-async";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../firebase/config";
 import { Link } from "react-router-dom";
-// import { sendEmailVerification } from "firebase/auth";
-import "./Home.css"
-import Modal from "../../shared/Modal";
-import { useState } from "react";
+import { sendEmailVerification } from "firebase/auth";
+// Level 3
+import "./Home.css";
 
+import { useState } from "react";
+import Modal from "../../shared/Modal";
 
 const Home = () => {
+  const [array, setarray] = useState([]);
+  const [subTask, setsubTask] = useState("");
+
+const addBTN = () => {
+  array.push(subTask)
+  console.log(array)
+  setsubTask("")
+}
+
+
+  
+
+
+
+
+
+
+
   const [user, loading, error] = useAuthState(auth);
-  console.log(user);
 
-  // const sendAgain = () => {
-  //   sendEmailVerification(auth.currentUser).then(() => {
-  //     console.log("Email verification sent!");
-  //     // ...
-  //   });
-  // };
 
+  const sendAgain = () => {
+    sendEmailVerification(auth.currentUser).then(() => {
+      console.log("Email verification sent!");
+      // ...
+    });
+  };
+
+  // LEVEL3
   const [showModal, setshowModal] = useState(false);
-
   const forgotPassword = () => {
     setshowModal(true);
   };
 
-const closeModal = () => { setshowModal(false) }
-
-
+  const closeModal = () => {
+    setshowModal(false);
+  };
 
   if (error) {
     return <Erroe404 />;
   }
-
 
   if (loading) {
     return <Loading />;
@@ -47,9 +65,7 @@ const closeModal = () => { setshowModal(false) }
       <>
         <Helmet>
           <title>HOME Page</title>
-          <style type="text/css">{`.Light main h1 span{color: #222}   `}
-          </style>
-
+          <style type="text/css">{`.Light main h1 span{color: #222}   `}</style>
         </Helmet>
 
         <Header />
@@ -77,6 +93,41 @@ const closeModal = () => { setshowModal(false) }
   }
 
   if (user) {
+    if (!user.emailVerified) {
+      return (
+        <>
+          <Helmet>
+            <title>HOME Page</title>
+            <meta name="description" content="HOMEEEEEEEEEEEE" />
+          </Helmet>
+
+          <Header />
+
+          <main>
+            <p>
+              {" "}
+              Welcome: {user.displayName}{" "}
+              <span>
+                <i className="fa-solid fa-heart"></i>{" "}
+              </span>
+            </p>
+
+            <p>Please verify your email to continue ✋ </p>
+            <button
+              onClick={() => {
+                sendAgain();
+              }}
+              className="delete"
+            >
+              Send email
+            </button>
+          </main>
+
+          <Footer />
+        </>
+      );
+    }
+
     if (user.emailVerified) {
       return (
         <>
@@ -87,77 +138,93 @@ const closeModal = () => { setshowModal(false) }
           <Header />
 
           <main className="home">
-          {/* OPTIONS */}
-           <section className="flex parent-of-btns mt">
+            {/* OPIONS (filtered data) */}
+            <section className="parent-of-btns flex mtt">
+              <button>Newest first</button>
 
-            <button>Newset </button>
-           
-            <button>Oldest </button>
- <select>
-              <option value="n1">All Tasks</option>
-              <option value="n1">Completed</option>
-              <option value="n1">Not Completed</option>
-
-            </select>
-           </section>
-
-           {/* SHOW ALL TASKS */}
-           <section className="mt flex all-tasks">
-            <article dir="auto" className="one-task">
-              <h2>New task</h2>
-              <ul><li>Sub task1 </li>
-              <li>Sub task2 </li></ul>
-              
-
-              <p className="time"> a day ago</p>
-            </article>
-            <article  dir="auto" className="one-task">
-              <h2>New task</h2>
-              <ul><li>Sub task1 </li>
-              <li>Sub task2 </li></ul>
-              
-
-              <p className="time"> a day ago</p>
-            </article>
-            <article dir="auto" className="one-task">
-              <h2>New task</h2>
-              <ul><li>Sub task1 </li>
-              <li>Sub task2 </li></ul>
-              
-
-              <p className="time"> a day ago</p>
-            </article>
-            <article dir="auto" className="one-task">
-              <h2>مهام اليوم</h2>
-              <ul><li>تمام </li>
-              <li>ماشي </li></ul>
-              
-
-              <p className="time"> a day ago</p>
-            </article>
-           </section>
-
-            {/* ADD NEW TASK BTN */}
-            <section className="mt">
-              <button  onClick={() => { setshowModal(true) }}  className="add-task-btn">ADD new task <i className="fa-solid fa-plus"></i></button>
+              <button>Oldest first</button>
+              <select id="browsers">
+                <option value="ddddd"> All Tasks </option>
+                <option value="dddddd"> Completed </option>
+                <option value="dddddd"> Not Completed </option>
+              </select>
             </section>
-{true && <Modal closeModal={closeModal}/>}
+
+            {/* SHOW all tasks */}
+            <section className="flex all-tasks mt">
+              <article dir="auto" className="one-task">
+                <Link to={"/edit-task"}>
+                  <h2> New Task </h2>
+                  <ul>
+                    <li>Sub task 1 </li>
+                    <li> Sub task 2</li>
+                  </ul>
+
+                  <p className="time">a day ago</p>
+                </Link>
+              </article>
+            </section>
+
+            {/* Add new task BTN */}
+            <section className="mt">
+              <button
+                onClick={() => {
+                  setshowModal(true);
+                }}
+                className="add-task-btn"
+              >
+                Add new task <i className="fa-solid fa-plus"></i>
+              </button>
+            </section>
+
+            {showModal && (
+              <Modal closeModal={closeModal}>
+                <div style={{ textAlign: "left" }}>
+                  <input
+                    onChange={(eo) => {}}
+                    required
+                    placeholder=" Add title : "
+                    type="text"
+                  />
+
+                  <div>
+                    <input
+                      onChange={(eo) => {
+
+                        setsubTask(eo.target.value)
+                      }}
+                      placeholder=" details "
+                      type="email"
+                      value={subTask}
+                    />
+
+                    <button
+                      onClick={(eo) => {
+                        eo.preventDefault();
+                        addBTN()
+                      }}
+                    >
+                      Add
+                    </button>
+                  </div>
+
+                  <ul>
+                    {array.map((item) => (
+                      <li key={item}>{item}</li>
+                    ))}
+                  </ul>
+
+                  <button
+                    onClick={(eo) => {
+                      eo.preventDefault();
+                    }}
+                  >
+                    Submit
+                  </button>
+                </div>
+              </Modal>
+            )}
           </main>
-          <Footer />
-        </>
-      );
-    }
-
-    if (!user.emailVerified) {
-      return (
-        <>
-          <Helmet>
-            <title>HOME Page</title>
-          </Helmet>
-
-          <Header />
-
-         
 
           <Footer />
         </>
@@ -167,3 +234,221 @@ const closeModal = () => { setshowModal(false) }
 };
 
 export default Home;
+
+
+
+// import Header from "../../comp/header";
+// import Footer from "../../comp/Footer";
+// import Loading from "../../comp/Loading";
+// import Erroe404 from "../erroe404";
+// import { Helmet } from "react-helmet-async";
+// import { useAuthState } from "react-firebase-hooks/auth";
+// import { auth } from "../../firebase/config";
+// import { Link } from "react-router-dom";
+// // import { sendEmailVerification } from "firebase/auth";
+// import "./Home.css";
+// import Modal from "../../shared/Modal";
+// import { useState } from "react";
+
+// const Home = () => {
+//   const [user, loading, error] = useAuthState(auth);
+//   console.log(user);
+
+//   const [array, setArray] = useState([]);
+//   const [showModal, setshowModal] = useState(false);
+//   const [subTask, setsubTask] = useState("");
+//   const forgotPassword = () => {
+//     setshowModal(true);
+//   };
+
+//   const addBtn = () => {
+//     array.push(subTask);
+//     console.log(array);
+//     setsubTask("");
+//   };
+
+//   const closeModal = () => {
+//     setshowModal(false);
+//   };
+
+//   if (error) {
+//     return <Erroe404 />;
+//   }
+
+//   if (loading) {
+//     return <Loading />;
+//   }
+
+//   if (!user) {
+//     return (
+//       <>
+//         <Helmet>
+//           <title>HOME Page</title>
+//           <style type="text/css">{`.Light main h1 span{color: #222}   `}</style>
+//         </Helmet>
+
+//         <Header />
+
+//         <main>
+//           <h1 style={{ fontSize: "28px" }}>
+//             {" "}
+//             <span>Welcome to React Level 2 🔥🔥🔥</span>{" "}
+//           </h1>
+//           <p className="pls">
+//             Please{" "}
+//             <Link style={{ fontSize: "30px" }} to="/signin">
+//               sign in
+//             </Link>{" "}
+//             to continue...{" "}
+//             <span>
+//               <i className="fa-solid fa-heart"></i>
+//             </span>
+//           </p>
+//         </main>
+
+//         <Footer />
+//       </>
+//     );
+//   }
+
+//   if (user) {
+//     if (user.emailVerified) {
+//       return (
+//         <>
+//           <Helmet>
+//             <title>HOME Page</title>
+//           </Helmet>
+
+//           <Header />
+
+//           <main className="home">
+//             {/* OPTIONS */}
+//             <section className="flex parent-of-btns mt">
+//               <button>Newset </button>
+
+//               <button>Oldest </button>
+//               <select>
+//                 <option value="n1">All Tasks</option>
+//                 <option value="n1">Completed</option>
+//                 <option value="n1">Not Completed</option>
+//               </select>
+//             </section>
+
+//             {/* SHOW ALL TASKS */}
+//             <section className="mt flex all-tasks">
+//               <article dir="auto" className="one-task">
+//                 <h2>New task</h2>
+//                 <ul>
+//                   <li>Sub task1 </li>
+//                   <li>Sub task2 </li>
+//                 </ul>
+
+//                 <p className="time"> a day ago</p>
+//               </article>
+//               <article dir="auto" className="one-task">
+//                 <h2>New task</h2>
+//                 <ul>
+//                   <li>Sub task1 </li>
+//                   <li>Sub task2 </li>
+//                 </ul>
+
+//                 <p className="time"> a day ago</p>
+//               </article>
+//               <article dir="auto" className="one-task">
+//                 <h2>New task</h2>
+//                 <ul>
+//                   <li>Sub task1 </li>
+//                   <li>Sub task2 </li>
+//                 </ul>
+
+//                 <p className="time"> a day ago</p>
+//               </article>
+//               <article dir="auto" className="one-task">
+//                 <h2>مهام اليوم</h2>
+//                 <ul>
+//                   <li>تمام </li>
+//                   <li>ماشي </li>
+//                 </ul>
+
+//                 <p className="time"> a day ago</p>
+//               </article>
+//             </section>
+
+//             {/* ADD NEW TASK BTN */}
+//             <section className="mt">
+//               <button
+//                 onClick={() => {
+//                   setshowModal(true);
+//                 }}
+//                 className="add-task-btn"
+//               >
+//                 ADD new task <i className="fa-solid fa-plus"></i>
+//               </button>
+//             </section>
+//             {showModal && (
+//               <Modal closeModal={closeModal}>
+//                 <div style={{ textAlign: "left" }}>
+//                   <input
+//                     onChange={(eo) => {}}
+//                     required
+//                     placeholder=" Add title "
+//                     type="title"
+//                   />
+//                   <div>
+//                     <input
+//                       onChange={(eo) => {
+//                         setsubTask(eo.target.value);
+//                       }}
+//                       placeholder=" details "
+//                       type="text"
+//                       value={subTask}
+//                     />
+//                     <button
+//                       onClick={(eo) => {
+//                         eo.preventDefault();
+//                         addBtn();
+//                       }}
+//                     >
+//                       Add
+//                     </button>
+//                   </div>
+
+//                   <ul>
+//                     {array.map((item) => (
+//                       <li key={item}>{item}</li>
+//                     ))}
+//                   </ul>
+
+//                   <button
+//                     onClick={(eo) => {
+//                       eo.preventDefault();
+//                     }}
+//                   >
+//                     Submit
+//                   </button>
+//                 </div>
+//               </Modal>
+//             )}
+//           </main>
+//           <Footer />
+//         </>
+//       );
+//     }
+
+//     if (!user.emailVerified) {
+//       return (
+//         <>
+//           <Helmet>
+//             <title>HOME Page</title>
+//           </Helmet>
+
+//           <Header />
+
+//           <Footer />
+//         </>
+//       );
+//     }
+//   }
+// };
+
+// export default Home;
--- react-task/src/shared/Modal.jsx
+++ react-task/src/shared/Modal.jsx
@@ -1,56 +1,66 @@
-import React, { useState } from 'react'
-import {
-    signInWithEmailAndPassword,
-    sendPasswordResetEmail,
-  } from "firebase/auth";
-import { auth } from '../firebase/config';
-export default function Modal({closeModal}) {
+import React from "react";
+import { Helmet } from "react-helmet-async";
 
+// closeModal => function to close the model
+const Modal = ({ closeModal, children }) => {
   return (
     <div className="parent-of-model">
-    <form className={`modal `}>
-    <div
-      onClick={() => {
-        closeModal()
-      }}
-      className="close"
-    >
-      <i className="fa-solid fa-xmark"></i>
+      <Helmet>
+        <style type="text/css">{`
+          
+          .parent-of-model{
+            position: fixed;
+            top: 0;
+            bottom: 0;
+            right: 0;
+            left: 0;
+          
+            background-color: rgba(0, 0, 0, 0.45);
+            display: flex;
+            align-items: center;
+            justify-content: center;
+          }
+
+          .modal{
+            background-color: whitesmoke;
+            width: 400px;
+            height: 333px;
+            border-radius: 12px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            position: fixed;
+            animation: mymove  0.8s;
+            overflow-y: scroll;
+
+          }
+          
+          
+          @keyframes mymove {
+            0%   {  scale: 0; transform: translateY(-100vh);}
+        
+            100% {  scale: 1; transform: translateY(0);}
+          } 
+          
+          
+          
+          `}</style>
+      </Helmet>
+
+      <form className={`modal`}>
+        <div
+          onClick={() => {
+            closeModal();
+          }}
+          className="close"
+        >
+          <i className="fa-solid fa-xmark"></i>
+        </div>
+
+        {children}
+      </form>
     </div>
+  );
+};
 
-    
-  </form>
-  </div>
-  )
-}
-
-
-// {/* <input
-//       onChange={(eo) => {
-//         setresetPass(eo.target.value);
-//       }}
-//       required
-//       placeholder=" E-mail : "
-//       type="email"
-//     />
-//     <button
-//       onClick={(eo) => {
-//         eo.preventDefault();
-
-//         sendPasswordResetEmail(auth, resetPass)
-//           .then(() => {
-//             console.log("send email");
-//             setshowSendEmail(true);
-//           })
-//           .catch((error) => {
-//             // ..
-//           });
-//       }}
-//     >
-//       Reset Password
-//     </button>
-//     {showSendEmail && (
-//       <p className="check-email">
-//         Please check your email to reset your password.
-//       </p>
-//     )} */}
+export default Modal;
